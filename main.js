52a4160c6b366a495b0161f3293c086050a3c71a
jack-kovensky <kovensky.j@northeastern.edu>
2023-01-30 16:24:47 -0500

    wrote the index.html and main.js files

---
+++ main.js
@@ -0,0 +1,63 @@
+//JS File for class demo
+// Jack Kovensky
+// 1.30.2023
+
+//print
+console.log("Hello World");
+
+// variables
+// constants
+const myCourse = "DS4200";
+console.log(myCourse);
+
+let season = "Winter";
+console.log(season);
+
+season = "Spring";
+console.log(season);
+
+
+// var
+
+var x = 7;
+// not to use Var to declare your variables
+
+console.log(typeof(season));
+
+let hwDue = true;
+console.log(typeof(hwDue));
+
+let faveNum = 6;
+console.log(typeof(faveNum));
+
+// loosely typed language
+faveNum = "six";
+console.log(typeof(faveNum));
+
+
+
+// fucntions
+
+function tenTimes(num){
+	let result = num * 10;
+	return result;
+}
+
+let ans = tenTimes(6);
+console.log(ans);
+
+
+let numberOfClicks = 0;
+
+function buttonClicked(){
+	
+	numberOfClicks += 1;
+
+	// select the element we want to modify
+	let buttonDiv = document.getElementById("button-div");
+
+	buttonDiv.innerHTML = "Number of times button clicked: " + numberOfClicks;
+
+}
+
+
