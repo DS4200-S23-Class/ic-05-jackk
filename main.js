30910b37c30f716609526f0de2de16f222042c4f
jack-kovensky <kovensky.j@northeastern.edu>
2023-01-31 12:28:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -57,8 +57,7 @@ function buttonClicked(){
 	let buttonDiv = document.getElementById("button-div");
 
 
-
-	buttonDiv.innerHTML = "Number of times button clicked" + numberOfClicks;
+	buttonDiv.innerHTML = "Number of times button clicked: " + numberOfClicks;
 
 }
 
